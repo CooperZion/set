b1f79975c074523c8fa55e3b42db67f56154e728
Cooper Zion <cooperzion2002@gmail.com>
2023-12-12 23:48:27 -0500

    Fixed problem with the vertex vector order

--- src/shapes/tex.cpp
+++ src/shapes/tex.cpp
@@ -27,10 +27,11 @@ void Tex::draw() const {
 void Tex::initVectors() {
     this->vertices.insert(vertices.end(), {
         // positions          // colors           // texture coords
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,    // top left
         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f   // bottom right
+
 
     });
 
